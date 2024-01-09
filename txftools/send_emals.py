780a658c99d1ea7fc58e667dd3ae3acbbd7aa763
txf <txf>
2024-01-09 19:09:23 +0800

    邮件优化

--- txftools/send_emals.py
+++ txftools/send_emals.py
@@ -29,25 +29,27 @@ class SendEmail(object):
         self.smtp_port = 465
         self.data_encoding = 'utf-8'
 
-    def send_email(self, data=None,  data_type='plain', data_header=None,
-                   from_header=None, to_addr=None, to_header=None, enclosure_file_list=None):
+    def send_email(self, data=None,
+                   data_type='plain',
+                   data_header=None,
+                   from_header=None,
+                   to_addr=None,
+                   to_header=None,
+                   enclosure_file_list=None,
+                   enclosure_file_dict_bytes=None
+                   ):
         """
         data: 要发送的数据
-
         收信方邮箱 单个用户可以为str 多用户使用list
         to_addr = ['yckj1987@126.com']
-
         data_type:  plain纯文本    html html数据类型
-
         data_header: 邮件标题
-
         from_header: 发件人
-
         to_header: 收件人 一般不用设置
-
-        enclosure_file_list: 附件文件列表
-
+        enclosure_file_list: 附件文件列表 [file1, file2, file3, ...]
+        enclosure_file_dict_bytes: 附件 {文件名1: 文件二进制1, 文件名2: 文件二进制2, ...}
         """
+
         # 创建一个带附件的实例msg
         msg = MIMEMultipart()
         # 邮件头信息
@@ -62,7 +64,7 @@ class SendEmail(object):
         # 邮箱正文内容，第一个参数为内容，第二个参数为格式(plain 为纯文本)，第三个参数为编码
         msg.attach(MIMEText(data, data_type, self.data_encoding))
 
-        # 邮件附件
+        # 邮件附件 文件名
         if enclosure_file_list:
             for file_ in enclosure_file_list:
                 # 构造附件
@@ -74,6 +76,14 @@ class SendEmail(object):
                 att.add_header('Content-Disposition', 'attachment', filename=file_name)
                 msg.attach(att)
 
+        # 邮件附件  二进制
+        if enclosure_file_dict_bytes:
+            for file_name, file_bytes in enclosure_file_dict_bytes.items():
+                # 构造附件
+                att = MIMEText(file_bytes, 'base64', 'utf-8')
+                att["Content-Type"] = 'application/octet-stream'
+                att.add_header('Content-Disposition', 'attachment', filename=file_name)
+                msg.attach(att)
 
         try:
             smtpobj = smtplib.SMTP_SSL(self.smtp_server)
@@ -94,10 +104,47 @@ class SendEmail(object):
                 pass
 
 
-s = SendEmail()
-# s.send_email(data='测试数据', to_addr=['yckj1987@126.com'])
-s.send_email(data="""
-<p>Python 邮件发送HTML格式文件测试...</p>
-<p><a href="http://www.runoob.com">这是一个链接</a></p>
-""", to_addr=['yckj1987@126.com'], data_type='html', enclosure_file_list=['12222.docx', '1222s2.docx'])
+class SendEmailQQ(SendEmail):
 
+    def __init__(self, from_addr, password):
+        super().__init__()
+        self.from_addr = from_addr
+        self.password = password
+
+
+def send_email_qq(
+        from_addr='402066270@qq.com',
+        password='就不告诉你',
+        data='测试数据123',
+        to_addr=['用户的邮箱'],
+        enclosure_file_list=['tests.py'],
+        enclosure_file_dict_bytes=None,
+        data_header='融道知变-删除数据'):
+    """
+        data 邮件文本内容
+        to_addr=用户邮箱列表
+        enclosure_file_list=附件列表
+    """
+
+    s = SendEmailQQ(from_addr, password)
+    return s.send_email(data=data,
+                        to_addr=to_addr,
+                        data_header=data_header,
+                        enclosure_file_list=enclosure_file_list,
+                        enclosure_file_dict_bytes=enclosure_file_dict_bytes
+                        )
+
+
+if __name__ == '__main__':
+
+    with open('tests.py', 'rb') as f:
+        enclosure_file_dict_bytes = f.read()
+
+    send_email_qq(
+        from_addr='402066270@qq.com',
+        password='lxupocvfhxxnbihb',
+        data='测试数据123',
+        to_addr=['yckj1987@126.com'],
+        # enclosure_file_list=['tests.py'],
+        enclosure_file_dict_bytes={'test.py': enclosure_file_dict_bytes},
+        data_header='融道知变-删除数据')
